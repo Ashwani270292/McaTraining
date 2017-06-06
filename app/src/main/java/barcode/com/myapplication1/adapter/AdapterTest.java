3da3e6712eb31077ec64de179a3073ecc93c6b36
ashwani270292 <ashwani270292@gmail.com>
2017-06-06 13:24:57 +0530

    Initial commit

---
+++ app/src/main/java/barcode/com/myapplication1/adapter/AdapterTest.java
@@ -0,0 +1,11 @@
+package barcode.com.myapplication1.adapter;
+
+/**
+ * Created by Divyank Vatshal on 06-06-2017.
+ */
+
+public class AdapterTest {
+
+
+
+}
---
+++ app/src/main/java/barcode/com/myapplication1/adapter/MyAdapter.java
@@ -0,0 +1,57 @@
+package barcode.com.myapplication1.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import barcode.com.myapplication1.R;
+
+/**
+ * Created by Divyank Vatshal on 06-06-2017.
+ */
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+    List<String> nameList;
+    Context context;
+
+    public MyAdapter(List<String> nameList, Context context){
+        this.nameList = nameList;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.card_name,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        String name = nameList.get(position);
+        holder.tvName.setText(name);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return nameList.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView tvName;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            tvName = (TextView)itemView.findViewById(R.id.tv_adapter_text);
+        }
+
+    }
+
+}
